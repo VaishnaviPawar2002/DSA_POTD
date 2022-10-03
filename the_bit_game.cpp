d484f858956566aba1b740f8de245731a3c41485
VaishnaviPawar2002 <96936136+VaishnaviPawar2002@users.noreply.github.com>
2022-10-03 20:26:14 +0530

    Add files via upload

---
+++ the_bit_game.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int swapBitGame(long long N){
+        // code here 
+         int cnt=0;
+        while(N!=0)
+        {
+            cnt=cnt+N&1;
+            N=N>>1;
+        }
+        if(cnt&1)
+        {
+            return 1;
+        }
+        else
+            return 2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.swapBitGame(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
